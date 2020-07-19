c33d4e676cb2803bfb5770b34bd41a0079371026
Dibu0404 <jaindivisha0404@gmail.com>
2020-07-19 18:02:14 +0530

    Initial commit

---
+++ src/MyFirstFile.kt
@@ -0,0 +1,14 @@
+import com.string.person
+
+
+fun main(args: Array<String>){
+
+
+  var personobj = person("Dibu")
+  personobj.display()
+
+
+}
+
+
+
---
+++ src/com/string/person.kt
@@ -0,0 +1,8 @@
+package com.string
+
+class person (var name: String){
+ fun display(){
+     print("The name of person is ${name}")
+ }
+
+}
